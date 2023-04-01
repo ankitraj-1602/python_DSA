c2450157497c99d595a70f70a4d72df3d6a7315d
Ankit Raj <nycankit@gmail.com>
2023-04-01 15:04:13 +0530

    sorting

---
+++ practice_31th.py
@@ -0,0 +1,7 @@
+# write a python function which accepts two linked list containing integer data and an integer n and merges two linked list such that list2 is merged with list 1 after n number of nodes 
+# assume that value of n will always be less than or equal to number of nodes in list1 
+#  1 2 4 3 5
+#  9 8 11
+#  n=2
+
+#  1 2 9 8 11 4 3 5
